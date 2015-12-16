1dd2c3eb7989b061f569616120b5c71d4bf6aa28
tjmiki <m_tojo@ncsa.jp>
2015-12-16 15:47:29 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-   
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+ 
+  def index
+      @users = User.all
+  end
+  
     def show
     @user = User.find(params[:id])
   end
@@ -11,17 +17,46 @@ class UsersController < ApplicationController
  def create
     @user = User.new(user_params)
     if @user.save
-flash[:success] = "Welcome to the Sample App!"
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
 
+   def edit
+   end
+
+     def update
+       if @user.update_attributes(user_params)
+            flash[:success] = "Profile updated"
+             redirect_to @user   
+       else
+         render 'edit'
+       end
+    end
+   def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+   end
+end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+        
+       # Before actions
+    def signed_in_user
+                unless signed_in?
+        store_location
+          redirect_to signin_url, notice: "Please sign in." 
+    end
+        end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
