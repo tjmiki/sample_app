3b8c978bd79f67a448a3649a5b5fcf4cc996ad54
tjmiki <m_tojo@ncsa.jp>
2015-12-16 17:37:19 +0900

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,6 +9,7 @@ end
 
 def sign_in(user, options={})
   if options[:no_capybara]
+    # Capybaraを使用していない場合にもサインインする。
     remember_token = User.new_remember_token
     cookies[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
